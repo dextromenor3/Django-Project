7287f73daacbf77f5ebe51ad4a158d485307d8d0
dextromenor3 <vladnov66@gmail.com>
2023-02-20 19:11:10 +0100

    Templates & Custom HTML
    Creating Templates and using inheritance in it (HTML)

--- main/views.py
+++ main/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Items
 
-def index(response):
-    return HttpResponse("Hello World, here we go once again, that\'s my Django Project")
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
 
 #def index(response, id):
  #   return HttpResponse("<h1>%s</h1>"% id)   #its a first way of a dynamic pages in terms of linking
 
-def list(response, id):
-    ls = ToDoList.objects.get(id=id)
-    item = ls.items_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))
+  
+def home(response):
+    return render(response, "main/home.html", {})
 
-# Create your views here.
